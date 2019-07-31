a5d3efbfe02a3f1341d51764d141e990060cb547
tanyujie <13551259347@139.com>
2019-07-31 16:38:11 +0800

    init

--- easymis-workflow-app/src/main/java/org/easymis/workflow/app/entity/bpm/BusinessObjectEntityRelationVO.java
+++ easymis-workflow-app/src/main/java/org/easymis/workflow/app/entity/bpm/BusinessObjectEntityRelationVO.java
@@ -2,7 +2,9 @@ package org.easymis.workflow.app.entity.bpm;
 
 import java.util.Date;
 
-public class BusinessObjectEntityRelation {
+import org.easymis.workflow.app.entity.vo.VO;
+
+public class BusinessObjectEntityRelationVO extends VO {
 	private String id;
 	private String orgId;
 	private String categoryId;
@@ -18,93 +20,123 @@ public class BusinessObjectEntityRelation {
 	private String foreignKey;
 	private String createId;
 	private Date createTime;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getOrgId() {
 		return orgId;
 	}
+
 	public void setOrgId(String orgId) {
 		this.orgId = orgId;
 	}
+
 	public String getCategoryId() {
 		return categoryId;
 	}
+
 	public void setCategoryId(String categoryId) {
 		this.categoryId = categoryId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDepict() {
 		return depict;
 	}
+
 	public void setDepict(String depict) {
 		this.depict = depict;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public Boolean getIsCreateTable() {
 		return isCreateTable;
 	}
+
 	public void setIsCreateTable(Boolean isCreateTable) {
 		this.isCreateTable = isCreateTable;
 	}
+
 	public String getTableName() {
 		return tableName;
 	}
+
 	public void setTableName(String tableName) {
 		this.tableName = tableName;
 	}
+
 	public String getDataSourceName() {
 		return dataSourceName;
 	}
+
 	public void setDataSourceName(String dataSourceName) {
 		this.dataSourceName = dataSourceName;
 	}
+
 	public String getPrimaryKeyType() {
 		return primaryKeyType;
 	}
+
 	public void setPrimaryKeyType(String primaryKeyType) {
 		this.primaryKeyType = primaryKeyType;
 	}
+
 	public Boolean getIsExternal() {
 		return isExternal;
 	}
+
 	public void setIsExternal(Boolean isExternal) {
 		this.isExternal = isExternal;
 	}
+
 	public String getPrimaryKey() {
 		return primaryKey;
 	}
+
 	public void setPrimaryKey(String primaryKey) {
 		this.primaryKey = primaryKey;
 	}
+
 	public String getForeignKey() {
 		return foreignKey;
 	}
+
 	public void setForeignKey(String foreignKey) {
 		this.foreignKey = foreignKey;
 	}
+
 	public String getCreateId() {
 		return createId;
 	}
+
 	public void setCreateId(String createId) {
 		this.createId = createId;
 	}
+
 	public Date getCreateTime() {
 		return createTime;
 	}
+
 	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
--- easymis-workflow-app/src/main/java/org/easymis/workflow/app/service/RoleService.java
+++ easymis-workflow-app/src/main/java/org/easymis/workflow/app/service/RoleService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 @Service
 @Transactional
 public class RoleService {
@@ -18,6 +21,7 @@ public class RoleService {
 	public Role findById(String roleId) {
 		return roleMapper.findById(roleId);
 	}
+
 	public boolean save(Role bean) {
 		boolean flag = false;
 		try {
@@ -32,7 +36,41 @@ public class RoleService {
 		}
 		return flag;
 	}
+
+	public boolean update(Role bean) {
+		boolean flag = false;
+		try {
+			bean.setRoleId(UUIDUtils.getUUID());
+			bean.setPriority(1);
+			bean.setIsSuper(false);
+			bean.setSystemDefault(false);
+			roleMapper.insertByBean(bean);
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+	public boolean delete(String[] ids) {
+		boolean flag = false;
+		try {
+			// roleMapper.insertByBean(bean);
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
 	public List<Role> findAll() {
 		return roleMapper.findAll();
+	}
+
+	public PageInfo getPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		List<Role> list = roleMapper.findAll();
+		PageInfo<Role> pageInfo = new PageInfo<Role>(list);
+		return pageInfo;
 	}
 }

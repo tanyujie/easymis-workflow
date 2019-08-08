1e29ab00b5c62afccc311847434e58df3342b966
tanyujie <13551259347@139.com>
2019-08-08 14:02:38 +0800

    init

---
+++ easymis-workflow-app/src/main/java/org/easymis/workflow/app/controller/project/KnowledgeGraphPropsController.java
@@ -0,0 +1,13 @@
+package org.easymis.workflow.app.controller.project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@Api(value = "组织分级管理设置 controller", tags = { "组织分级管理设置  操作接口" })
+@RequestMapping("/knowledge/graph/props")
+public class KnowledgeGraphPropsController {
+
+}

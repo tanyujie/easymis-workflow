b489e6bc12466b71f859515e2d32fd1e205714ad
tanyujie <13551259347@139.com>
2019-08-31 13:45:06 +0800

    init

--- easymis-workflow-elasticsearch/src/main/java/org/easymis/workflow/elasticsearch/sort/SortV2.java
+++ easymis-workflow-elasticsearch/src/main/java/org/easymis/workflow/elasticsearch/sort/SortV2.java
@@ -10,24 +10,21 @@ public class SortV2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//将第i位上的元素与其后的每一个元素分别比较，每次遇见更小的，就将两个元素交换位置
-		//第i位上的元素确定之后再继续确定第i+1位上的元素
-		int arr2[]= {23,48,12,56,45};
-			int temp;
-			for(int i=0;i<arr2.length;i++)
-			{
-				for(int j=i+1;j<arr2.length;j++)
-				{
-					if(arr2[i]>arr2[j])
-					{
-						temp=arr2[i];
-						arr2[i]=arr2[j];
-						arr2[j]=temp;
-					}
+		// 将第i位上的元素与其后的每一个元素分别比较，每次遇见更小的，就将两个元素交换位置
+		// 第i位上的元素确定之后再继续确定第i+1位上的元素
+		int arr2[] = { 23, 48, 12, 56, 45 };
+		int temp;
+		for (int i = 0; i < arr2.length; i++) {
+			for (int j = i + 1; j < arr2.length; j++) {
+				if (arr2[i] > arr2[j]) {
+					temp = arr2[i];
+					arr2[i] = arr2[j];
+					arr2[j] = temp;
 				}
 			}
-			for (int i = 0; i < arr2.length; i++)
-				System.out.print(arr2[i]+",");
+		}
+		for (int i = 0; i < arr2.length; i++)
+			System.out.print(arr2[i] + ",");
 	}
 
 }

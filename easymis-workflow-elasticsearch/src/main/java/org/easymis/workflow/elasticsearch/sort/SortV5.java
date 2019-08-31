78d741510645d867510299aca18fdfdf84b06c7f
tanyujie <13551259347@139.com>
2019-08-31 15:17:42 +0800

    init

--- easymis-workflow-elasticsearch/src/main/java/org/easymis/workflow/elasticsearch/sort/SortV5.java
+++ easymis-workflow-elasticsearch/src/main/java/org/easymis/workflow/elasticsearch/sort/SortV5.java
@@ -5,19 +5,22 @@ package org.easymis.workflow.elasticsearch.sort;
 public class SortV5 {
 
 	public static void main(String[] args) {
-		int []arr5={23,12,48,56,45};
+		int[] arr5 = { 99,23, 120, 48, 56, 45 };
 		for (int i = 1; i < arr5.length; i++) {
 			for (int j = i; j > 0; j--) {
-				if (arr5[j - 1] > arr5[j]) {//大的放后面
+				if (arr5[j - 1] > arr5[j]) {// 大的放后面
 					int tmp = arr5[j - 1];
 					arr5[j - 1] = arr5[j];
 					arr5[j] = tmp;
 				}
 			}
+			for (int k = 0; k < arr5.length; k++)
+				System.out.print(arr5[k] + ",");
+			System.out.println("!");
 		}
 
-			for (int i = 0; i < arr5.length; i++)
-				System.out.print(arr5[i]+",");
+		for (int i = 0; i < arr5.length; i++)
+			System.out.print(arr5[i] + ",");
 	}
 
 }
